70d3c11b9dee02690b93d5652c8d5cdda74b0fc8
Nune <Nune@Nunes-MacBook-Pro.local>
2016-04-17 19:34:26 +0700

    add spacebarAction

--- src/Spacebar.js
+++ src/Spacebar.js
@@ -5,5 +5,11 @@ var Spacebar = cc.Sprite.extend({
     ctor: function() {
         this._super();
         this.initWithFile( 'res/images/spacebar1.png' );
+        var animation = new cc.Animation.create();
+        animation.addSpriteFrameWithFile( 'res/images/spacebar1.png' );
+        animation.addSpriteFrameWithFile( 'res/images/spacebar2.png' );
+        animation.setDelayPerUnit( 0.2 );
+        var spacebarAction = cc.RepeatForever.create( cc.Animate.create( animation ) );
+        this.runAction( spacebarAction );
     }
 });
